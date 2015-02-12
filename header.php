87c284aa5180150708ac4e717ae3663a296320a4
Pratik <pratikc@brainstormforce.com>
2015-02-12 14:31:45 +0530

    Added localhost open sans font

--- header.php
+++ header.php
@@ -21,7 +21,6 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
