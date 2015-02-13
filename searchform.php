f969516f6c56104bc031d6d9b7852b0b401d11a8
Pratik <pratikc@brainstormforce.com>
2015-02-13 20:23:43 +0530

    reset

--- searchform.php
+++
@@ -1,8 +0,0 @@
-<form action="<?php echo home_url(); ?>/" method="get" id="searchform">
-    <fieldset>
-        <div id="searchbox">
-            <input class="input" name="s" type="text" id="s" value="<?php _e( 'type here...' , 'ultimate' ); ?>" onfocus="if (this.value == '<?php _e( 'type here...' , 'ultimate' ) ?>') {this.value = '';}" onblur="if (this.value == '') {this.value = '<?php _e( 'type here...' , 'ultimate' ) ?>';}">
-            <button type="submit" id="searchsubmit" class="ultimate-bkg ultimate-bkg-dark-hover"><i class="ent entsearch"></i></button>
-        </div>
-	</fieldset>
-</form>
