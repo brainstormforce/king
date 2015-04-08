24e6fc14ba8e41be6a0d23525d0ad0f91f62711e
Pratik <pratikc@brainstormforce.com>
2015-04-08 14:55:55 +0530

    Fixed animation issue

--- inc/king-animation.php
+++ inc/king-animation.php
@@ -7,68 +7,76 @@
  */
 $blog_animation = get_theme_mod('blog_animation', 'fadeIn');
 if($blog_animation != 'none') :
-    if ( is_home() || is_archive() || is_search() ) : 
+    if ( ! function_exists( 'king_animation_before_wrapper' ) ) :
         add_action('king_content_top','king_animation_before_wrapper');
         function king_animation_before_wrapper() {
-            echo '<div class="king-animation-wrapper" data-king-animate="'. get_theme_mod('blog_animation', 'fadeIn') .'">';
+            if ( is_home() || is_archive() || is_search() ) : 
+                echo '<div class="king-animation-wrapper" data-king-animate="'. get_theme_mod('blog_animation', 'fadeIn') .'">';
+            endif;
         }
+    endif;
+    if ( ! function_exists( 'king_animation_after_wrapper' ) ) :
         add_action('king_content_bottom','king_animation_after_wrapper');
         function king_animation_after_wrapper() {
-            echo '</div>';
+            if ( is_home() || is_archive() || is_search() ) : 
+                echo '</div>';
+            endif;
         }
     endif;
 endif;
 
-function king_animations_callback() {
-    $blog_animation = get_theme_mod('blog_animation', 'fadeIn');
-    if($blog_animation != 'none') :
-        if ( is_home() || is_archive() || is_search() ) : 
-        ?>
-            <script type="text/javascript">
-                (function($) {
-                    "use strict";
-                    $.fn.kingsIsAppear = function(options) { 
-                        var defaults = {
-                            viewport: 90, //in percentage
-                        };
-            
-                        var options = $.extend(defaults, options);
-                        var ws = jQuery(window).scrollTop();
-                        var wh = $(window).height();
-                        var viewport = 100 - options.viewport;
-                        var viewport_pixel = wh - (wh * (viewport/100));
-                        var offset = $(this).offset().top;
-                        var position = offset - ws;
-                        if(position <= viewport_pixel)
-                            return true;
-                        else
-                            return false;
-                    }
-                    
-                    function kingsAnimateBlog() {
-                        var animate = $('.king-animation-wrapper').attr('data-king-animate');
-                        $('.king-animation-wrapper').find('article').each(function(i,blog){
-                            if(typeof animate === 'undefined' || animate === '')
-                                return;
-                            var appear = $(blog).kingsIsAppear();
-                            $('#content').find('article').css('opacity',0);
-                            if((typeof animate !== 'undefined' && animate !== '') && appear) {
-                                $(blog).addClass('animated').addClass(animate).css('opacity',1);
-                            }
-                        });
-                    }
-                    
-                    $(document).ready(function(){
-                        kingsAnimateBlog();
-                    });
-                    $(window).scroll(function(){
-                        kingsAnimateBlog();
-                    });
+if ( ! function_exists( 'king_animations_callback' ) ) :
+    function king_animations_callback() {
+        $blog_animation = get_theme_mod('blog_animation', 'fadeIn');
+        if($blog_animation != 'none') :
+            if ( is_home() || is_archive() || is_search() ) : 
+            ?>
+                <script type="text/javascript">
+                    (function($) {
+                        "use strict";
+                        $.fn.kingsIsAppear = function(options) { 
+                            var defaults = {
+                                viewport: 90, //in percentage
+                            };
+                
+                            var options = $.extend(defaults, options);
+                            var ws = jQuery(window).scrollTop();
+                            var wh = $(window).height();
+                            var viewport = 100 - options.viewport;
+                            var viewport_pixel = wh - (wh * (viewport/100));
+                            var offset = $(this).offset().top;
+                            var position = offset - ws;
+                            if(position <= viewport_pixel)
+                                return true;
+                            else
+                                return false;
+                        }
                         
-                })(jQuery);
-            </script>
-        <?php
+                        function kingsAnimateBlog() {
+                            var animate = $('.king-animation-wrapper').attr('data-king-animate');
+                            $('.king-animation-wrapper').find('article').each(function(i,blog){
+                                if(typeof animate === 'undefined' || animate === '')
+                                    return;
+                                var appear = $(blog).kingsIsAppear();
+                                $('#content').find('article').css('opacity',0);
+                                if((typeof animate !== 'undefined' && animate !== '') && appear) {
+                                    $(blog).addClass('animated').addClass(animate).css('opacity',1);
+                                }
+                            });
+                        }
+                        
+                        $(document).ready(function(){
+                            kingsAnimateBlog();
+                        });
+                        $(window).scroll(function(){
+                            kingsAnimateBlog();
+                        });
+                            
+                    })(jQuery);
+                </script>
+            <?php
+            endif;
         endif;
-    endif;
-}
-add_action('wp_footer', 'king_animations_callback');
+    }
+    add_action('wp_footer', 'king_animations_callback');
+endif;
